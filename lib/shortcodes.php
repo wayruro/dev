e1bf0fa088cf68cf0fb0446ae8bf7d7d2a899187
spartan <user@192.168.1.170>
2015-08-18 15:29:44 -0400

    Upd
    Added

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -8,3 +8,24 @@ function foobar_func( $atts ){
 
 }
 add_shortcode( 'breadcrumbz', 'foobar_func' );
+
+
+function spartan_home_url() { // lets avoid rewriting URLs when setting this puppy live - rj - may 6, 2015
+
+  // $home_urls = site_url() . '/';
+  // $home_urls = site_url();
+  $home_urls = home_url();
+  return $home_urls;
+
+}
+add_shortcode('home', 'spartan_home_url'); // [home] wherever you wanna call the home page URL
+
+function spartan_wp_url() { // lets avoid rewriting URLs when setting this puppy live - rj - may 6, 2015
+
+  // $home_urls = site_url() . '/';
+  $home_urls = site_url();
+  // $home_urls = home_url();
+  return $home_urls;
+
+}
+add_shortcode('wp', 'spartan_wp_url'); // [home] wherever you wanna call the home page URL
