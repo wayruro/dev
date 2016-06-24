f928a1eafc89acf65c6b930ad4678005172ffde6
Wayruro <hello@wayruro.com>
2016-06-24 18:09:32 -0400

    updt

--- lib/options.php
+++ lib/options.php
@@ -1,7 +1,7 @@
 <?php
 /* Options and settings
   ================================================== */
-define ('WP_POST_REVISIONS', false ) ;
+
 define ('WP_POST_REVISIONS', 0 );
 //Remove revisions actions in post.php
 remove_action('pre_post_update', 'wp_save_post_revision');
@@ -11,6 +11,3 @@ $wpdb->query( "
     DELETE FROM $wpdb->posts
     WHERE post_type = 'revision'
 " );
-
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
