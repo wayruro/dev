acc042d1d1626fdc2cc635064d39ce991fed8f22
Wayruro <hello@wayruro.com>
2016-09-30 16:29:07 -0400

    updt

--- under-construction.php
+++ under-construction.php
@@ -1,10 +1,55 @@
+<style type="text/css">
+  body {
+  margin: 0;
+  font-family: "Myriad Set Pro","Helvetica Neue","Helvetica","Arial",sans-serif;
+  font-size:100%;
+  line-height:1.5;
+  height:100%;
+  color: #fff;
+  border-top: 2px solid #00a0d2;
+  background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAQACAIAAAAP+8yGAAACoklEQVR4Xu3dwQ0CMQxEUTtKvbRCC3RqjtwR3g3WSwP/MDOObaIlH89XdJ4VzQcAACBiZzcgMvs1AAAAAFCu3WiR3YCYcKMBACh2AKopgFJBA7Xoo4Fip1TQAEAtkmQASXZlalsAJLk/aC59AICdSQNJ7ge4DwRN0ORAbypociAHgsamliHWmlwkB7oKnZ0caH7lQA40v3LApmqRHPiBAsBCSvsOYCHFpuZkfZEcsCmbsqn2/XSbchGAHLApm3q9D6Cz07YAaB19oZWLaCAHbHoiwBf52BRADtiUTb1KAJAD384B8Gkbzx4APOyWAwAjlKCx6eQky4GgyYGgCZocAAiapSCAoLkPJBnA03QASZZkGgAoFVpHczKAWmQIVIsAAOxNARQ7AAB9kUEcQLn+/gCopgAAbjQ3Gg1cOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANV8aAAAAADwi7Pr/5+DVt2tAQAAgGK3I6oZUANEBgDQtigVACFoYQBhUwBBEzQAORA0NmVTOWBTADYlMptyEQCbEhnAOzsALjpPA88e6rJ3FUTmIi7Sm3IRF3ERkS1miaxUEFmpILJSURN6UyIT2QhlALHxIjKRiUxk+yKTPpGJbNInMpEN4sMARAawDBG06SIL2goi21UIGpFpcJlNjbFENuEQmQY0MGUaQEyZRBY0QRM0U6YpkwZEFjTrHFMmDTS/RCayKdOUacoUNH+4I2hENmX2AIhMZGMskWlgjCUyke2LNF5EJjINiExkGy8bL8uQCSIbxIlspcZFJn0iE1mpsEogsjGWi7iIi4jMRVzERfamAJaC52sAwEUANl5sCsCmXGRG89e6bEoDOWBTADmQAwBBk4MRAABJFjQAAIBqPvdrAPAGv7GBbaSCtb0AAAAASUVORK5CYII=");
+  background-attachment: fixed;
+  background-size: contain;
+  box-sizing: border-box;
+}
+
+#login {
+    width: 300px;
+    height: 300px;
+    padding: 10px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    margin-top: -150px;
+    margin-left: -150px;
+}
+
+h1 {
+  text-shadow: 0.5px 0.5px 2px rgba(0, 0, 0, 0.3);
+}
+
+#login:after {
+    content: 'Site Crafted in Orlando - Florida';
+    text-align: center;
+    text-indent: 0 !important;
+    display: block;
+    width: 100%;
+    font-size: 0.8rem;
+    color: #5F7883;
+    position: relative;
+    bottom: -50%;
+}
+</style>
+
+<div id="login" class="content-area" style="text-align: center;">
+
+    <main id="main" class="site-main" role="main">
+
+      <h1 style="color: #fff; font-size: 1.75rem; font-weight: 300; letter-spacing: 1px;">Be Ready, We Are Launching Soon.</h1>
+
+    </main>
+
+</div>
 
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<h1>Holas</h1>
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
 
